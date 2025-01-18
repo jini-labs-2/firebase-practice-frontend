b4be968a800e13f625d363079e025c70fb0f0ef4
und-jini <choiyongjin@undershaft.net>
2025-01-19 00:07:31 +0900

    get file 一覧

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Fileupload from './pages/fileupload';
+import Filelist from './pages/filelist';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/signup" element={<Signup />}> </Route>
       <Route path="/signin" element={<Signin />}> </Route>
       <Route path="/upload" element={<Fileupload />}> </Route>
+      <Route path="/filelist" element={<Filelist />}> </Route>
     </Routes>
   );
 }
---
+++ src/pages/filelist.tsx
@@ -0,0 +1,59 @@
+import { getStorage, listAll, ref } from 'firebase/storage';
+import React, { useState } from 'react'
+
+interface FileList {
+  name: string,
+  bucket: string,
+  fullPath: string,
+}
+
+const Filelist = () => {
+  const [filelist, setFilelist] = useState<FileList[]>([]);
+  const [queryfile, setQueryfile] = useState('');
+
+  const getFileList = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    setFilelist([]);
+    const storageRef = getStorage();
+    const listRef = ref(storageRef, queryfile);
+    listAll(listRef)
+      .then((res) => {
+        console.log(res);
+        res.prefixes.forEach((folderRef) => {
+          console.log('--folderRef--', folderRef)
+        });
+        
+        const itemList = res.items.map((item) => {
+          const {name, bucket, fullPath} = item;
+          return {name, bucket, fullPath}
+        })
+        setFilelist(itemList)
+      })
+      .catch((err) => console.error(err))
+    }
+
+  return (
+    <div>
+      <input type='text' value={queryfile} onChange={(e) => setQueryfile(e.target.value)}/>
+      <button onClick={getFileList}>Get File List</button>
+      <p>件数：{filelist.length}</p>
+      <div style={{padding: '0.5rem', display:'flex', justifyContent: 'space-between', color: 'black', fontWeight: '800'}}> 
+        <span> name </span>
+        <span> fullPath </span>
+        <span> bucket </span>
+      </div>
+      {
+        filelist ?  filelist.map((item, index) => (
+          <div key={index} style={{padding: '0.5rem', display:'flex', justifyContent: 'space-between'}}> 
+            <span> {item.name} </span>
+            <span> {item.fullPath} </span>
+            <span> {item.bucket} </span>
+          </div>
+        )) : ''
+      }
+    </div>
+  )
+}
+
+export default Filelist
